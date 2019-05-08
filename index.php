25ee038356dab46fd01ef47e062f5b9359e1b33e
LOPEZ HENRY <150036653@cart.ads>
2019-05-08 15:01:18 -0700

    created borders

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Home</title>
+    <link rel="stylesheet" href="css/background.css">
+    <link rel="stylesheet" href="js/java.js">
+     <meta charset="UTF-8">
+<title>Home</title>
 </head>
 <body>
-    
+
+<div class="main">
+ 
+ <div class="lily">
+     <p>bgfdsjhfsddsf</p>
+ </div>
+<div class="wood">
+    <p>hjsdfhf</p>
+</div>
+ 
+</div>
+
+
 </body>
 </html>
--- plans.php
+++ plans.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+   <link rel="stylesheet" href="css/background.css">
+   <link rel="stylesheet" href="js/java.js">
     <meta charset="UTF-8">
     <title>Plans</title>
 </head>
